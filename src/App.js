7137a3719e27960413cf115f152680fa269407c3
Exeldinho <exeldinho@gmail.com>
2021-04-20 17:43:23 +0300

    Start on frontend. Form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './App.css';
 //import ReactDOM from 'react-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 import Form from './components/form';
 import RecentActivities from "./components/recentActivities";
 import Achievements from "./components/achievements";
@@ -10,6 +11,7 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
+            <header>Activity tracker</header>
                 <Form/>
 
             </div>
--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class Form extends Component {
     constructor(props) {
@@ -11,10 +13,10 @@ export default class Form extends Component {
         this.onSubmit = this.onSubmit.bind(this);
 
         this.state = {
-            activityStart: new Date(),
-            activityFinish: new Date(),
-            distance: 0,
-            activityType: []
+            activityStart: '',
+            activityFinish: '',
+            distance: '',
+            activityType: ''
         }
     }
 
@@ -63,12 +65,61 @@ export default class Form extends Component {
     render() {
         return (
             <div className="form">
+                <form onSubmit={this.onSubmit}>
 
+                    <span className="form-group">
+                    <label>Add new activity:</label>
+                    <DatePicker className="form-group"
+                        selected={this.state.activityStart}
+                        onChange={this.onChangeActivityStart}
+                        placeholderText={"Start time"}
+                        showTimeSelect
+                      //  timeFormat="HH:mm"
+                        timeIntervals={1}
+                        dateFormat="HH:mm" //DD/MM/YYYY HH:mm:ss d MMM Y HH:mm
+                    />
+                    </span>
 
+                    <span className="form-group">
+                    <DatePicker className="form-group"
+                        selected={this.state.activityFinish}
+                        onChange={this.onChangeActivityFinish}
+                        placeholderText="Finish time"
+                        showTimeSelect
+                       // timeFormat="HH:mm"
+                        timeIntervals={1}
+                        dateFormat="HH:mm" //DD/MM/YYYY HH:mm:ss d MMM Y HH:mm
+                    />
+                    </span>
 
+                    <span className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={this.state.distance}
+                            onChange={this.onChangeDistance}
+                            placeholder="Distance"
+                        />
+                    </span>
 
+                    <span className="form-group">
+                        <select
+                            placeholder="Select activity type"
+                            required
+                            value={this.state.activityType}
+                            onChange={this.onChangeActivityType}>
+                        </select>
+                    </span>
 
-                <p>Form component</p>
+                    <span className="form-group">
+                        <input
+                            type="submit"
+                            value="Save"
+                            className="button"
+                            />
+                    </span>
+
+                </form>
             </div>
         )
     }
