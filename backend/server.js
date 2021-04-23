3e1fb0ad8fdb2f82b864f727bf9ed20cf00b13a5
Exeldinho <exeldinho@gmail.com>
2021-04-23 10:04:03 +0300

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-require('dotenv').config();
+require('dotenv').config()
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect (uri, { useNewUrlParser: true, useCreateIndex: true});
-const connection = mongoose.connection;
+const uri = process.env.ATLAS_URI
+mongoose.connect (uri, { useNewUrlParser: true, useCreateIndex: true})
+const connection = mongoose.connection
 connection.once('open', () => {
-    console.log ('MongoDB database connected');
+    console.log ('MongoDB database connected')
 })
 
-const activitiesRouter = require('./routes/activities');
+const activitiesRouter = require('./routes/activities')
 
-app.use ('/', activitiesRouter);
+app.use ('/', activitiesRouter)
 
 app.listen(port, () => {
-    console.log (`Server is started on port: ${port}`);
-});
+    console.log (`Server is started on port: ${port}`)
+})
